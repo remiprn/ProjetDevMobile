75ee932dc7dd4b5d9ed3783fdb5d78e746a8d9ec
Rémi <remi.pierron9@etu.univ-lorraine.fr>
2019-02-09 10:06:50 +0100

    premier commit

---
+++ ProjetDevMobile/ProjetDevMobile/ViewModels/MenuViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetDevMobile.ViewModels
+{
+    public class MenuViewModel : ViewModelBase
+    {
+        public DelegateCommand<string> NavigateCommand { get; private set; }
+
+        public MenuViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string view)
+        {
+            NavigationService.NavigateAsync("NavigationPage/" + view);
+        }
+    }
+}
---
+++ ProjetDevMobile/ProjetDevMobile/Views/Menu.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ProjetDevMobile.Views
+{
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
