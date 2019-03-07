61a13eead0fe3d02820fb4300d71a4da5534d061
EnzoJ57 <cod.enzo57gmail.com>
2019-03-07 13:30:45 +0100

    enregistrements:ajout date

---
+++ ProjetDevMobile/ProjetDevMobile/Services/CalculeDateConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ProjetDevMobile.Services
+{
+    class CalculeDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan diff = DateTime.Now.Subtract((DateTime)value);
+            double nbJour = diff.TotalDays;
+            int nbJourInt = System.Convert.ToInt32(nbJour)-1;
+            if(nbJourInt == 0)
+            {
+                return "Today";
+            }
+            else if(nbJourInt < 7)
+            {
+                return nbJourInt + " jours";
+            }
+            else
+            {
+                int nbSemaine = nbJourInt / 7;
+                return nbSemaine + " semaines";
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
