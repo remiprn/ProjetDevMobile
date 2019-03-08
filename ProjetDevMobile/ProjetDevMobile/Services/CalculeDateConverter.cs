af6ae1eb668baefe549049ac4afa387efc8ee08e
EnzoJ57 <cod.enzo57gmail.com>
2019-03-08 02:13:20 +0100

    design

--- ProjetDevMobile/ProjetDevMobile/Services/CalculeDateConverter.cs
+++ ProjetDevMobile/ProjetDevMobile/Services/CalculeDateConverter.cs
@@ -10,9 +10,9 @@ namespace ProjetDevMobile.Services
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            TimeSpan diff = DateTime.Now.Subtract((DateTime)value);
+            TimeSpan diff = DateTime.Today.Subtract((DateTime)value);
             double nbJour = diff.TotalDays;
-            int nbJourInt = System.Convert.ToInt32(nbJour)-1;
+            int nbJourInt = System.Convert.ToInt32(nbJour);
             if(nbJourInt == 0)
             {
                 return "Today";
